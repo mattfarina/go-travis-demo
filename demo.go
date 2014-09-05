f25c28c87541eb013e6abbdd04ad840420f0e2f9
Matt Farina <matt@mattfarina.com>
2014-09-05 14:43:01 -0400

    Initial commit.

---
+++ demo.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func getFormatString() string {
+	return "Hi there, I love %s!!"
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	format := getFormatString()
+	fmt.Fprintf(w, format, r.URL.Path[1:])
+}
+
+func main() {
+	addr := ":" + os.Getenv("PORT")
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(addr, nil)
+}
---
+++ demo_test.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestFormatString(t *testing.T) {
+	format := getFormatString()
+
+	if format != "Hi there, I love %s!!" {
+		t.Error("The format string is broken.")
+	}
+}
